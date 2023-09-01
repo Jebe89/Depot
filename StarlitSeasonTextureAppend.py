bb770415cbff23507365d8ff8fc3a5aac647400c
Jebe89 <38123399+Jebe89@users.noreply.github.com>
2023-09-01 21:36:48 +0800

    Add files via upload

--- StarlitSeasonTextureAppend.py
+++ StarlitSeasonTextureAppend.py
@@ -9,58 +9,64 @@ lib_dir = r'F:/UE4/starlitseason_archive/'
 coll = bpy.data.collections.get(collname)
 for obj in coll.objects:
     #print(obj.name)
+    lodcheck = obj.name.__contains__('Lod1') + obj.name.__contains__('Lod2') + obj.name.__contains__('Lod3')
+    if lodcheck != 0:
+        print(obj.name + 'is Lod')
+        continue
+        
+    else:
+    
+        meshfile = open(mesh_dir + obj.name + '.json')
+        meshdata = json.load(meshfile)
 
-    meshfile = open(mesh_dir + obj.name + '.json')
-    meshdata = json.load(meshfile)
+        for i in meshdata:
+            meshtype = i['Type']
 
-    for i in meshdata:
-        meshtype = i['Type']
+            if meshtype == 'StaticMesh':
+                mat = i['Properties']['StaticMaterials']
 
-        if meshtype == 'StaticMesh':
-            mat = i['Properties']['StaticMaterials']
+                for u in mat:
+                    matname = str(u['MaterialInterface']['ObjectName']).replace("MaterialInstanceConstant'","").replace("'","")
+                    matdir = str(u['MaterialInterface']['ObjectPath']).replace(".0",".json")
+                    #print(matname)
+                    #print(matdir)
 
-            for u in mat:
-                matname = str(u['MaterialInterface']['ObjectName']).replace("MaterialInstanceConstant'","").replace("'","")
-                matdir = str(u['MaterialInterface']['ObjectPath']).replace(".0",".json")
-                #print(matname)
-                #print(matdir)
+                    for bmat in bpy.data.materials:
+                        if matname == bmat.name:
 
-                for bmat in bpy.data.materials:
-                    if matname == bmat.name:
+                            bmat.use_nodes = True
+                            matfile = open(lib_dir + matdir)
+                            matdata = json.load(matfile)
+                            matdata = matdata[0]['Properties']
 
-                        bmat.use_nodes = True
-                        matfile = open(lib_dir + matdir)
-                        matdata = json.load(matfile)
-                        matdata = matdata[0]['Properties']
+                            if matdata.get('TextureParameterValues') != None:
+                                matdata = matdata['TextureParameterValues']
+                                #print(matdata)
 
-                        if matdata.get('TextureParameterValues') != None:
-                            matdata = matdata['TextureParameterValues']
-                            #print(matdata)
+                                for o in matdata:
 
-                            for o in matdata:
+                                    if o['ParameterInfo']['Name'] == 'defTex' or o['ParameterInfo']['Name'] == 'DefTex':
 
-                                if o['ParameterInfo']['Name'] == 'defTex' or o['ParameterInfo']['Name'] == 'DefTex':
+                                        deftex_path = str(o['ParameterValue']['ObjectPath']).replace(".0",".tga")
+                                        deftex = bpy.data.images.load(lib_dir + deftex_path)
+                                        deftex.alpha_mode = 'NONE'
 
-                                    deftex_path = str(o['ParameterValue']['ObjectPath']).replace(".0",".tga")
-                                    deftex = bpy.data.images.load(lib_dir + deftex_path)
-                                    deftex.alpha_mode = 'NONE'
+                                        principled_BSDF = bmat.node_tree.nodes['Principled BSDF']
+                                        deftex_node = bmat.node_tree.nodes.new('ShaderNodeTexImage')
+                                        deftex_node.image = deftex
+                                        bmat.node_tree.links.new(deftex_node.outputs[0], principled_BSDF.inputs[0])
 
-                                    principled_BSDF = bmat.node_tree.nodes['Principled BSDF']
-                                    deftex_node = bmat.node_tree.nodes.new('ShaderNodeTexImage')
-                                    deftex_node.image = deftex
-                                    bmat.node_tree.links.new(deftex_node.outputs[0], principled_BSDF.inputs[0])
+                                        print(matname)
 
-                                    print(matname)
+                                    if o['ParameterInfo']['Name'] == 'emiTex' or o['ParameterInfo']['Name'] == 'EmiTex':
 
-                                if o['ParameterInfo']['Name'] == 'emiTex' or o['ParameterInfo']['Name'] == 'EmiTex':
+                                        emitex_path = str(o['ParameterValue']['ObjectPath']).replace(".0",".tga")
+                                        emitex = bpy.data.images.load(lib_dir + emitex_path)
+                                        emitex.alpha_mode = 'NONE'
 
-                                    emitex_path = str(o['ParameterValue']['ObjectPath']).replace(".0",".tga")
-                                    emitex = bpy.data.images.load(lib_dir + emitex_path)
-                                    emitex.alpha_mode = 'NONE'
-
-                                    principled_BSDF = bmat.node_tree.nodes['Principled BSDF']
-                                    emitex_node = bmat.node_tree.nodes.new('ShaderNodeTexImage')
-                                    emitex_node.image = emitex
-                                    bmat.node_tree.links.new(emitex_node.outputs[0], principled_BSDF.inputs[19])
+                                        principled_BSDF = bmat.node_tree.nodes['Principled BSDF']
+                                        emitex_node = bmat.node_tree.nodes.new('ShaderNodeTexImage')
+                                        emitex_node.image = emitex
+                                        bmat.node_tree.links.new(emitex_node.outputs[0], principled_BSDF.inputs[19])
 
 
